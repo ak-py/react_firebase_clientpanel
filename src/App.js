86cea4f37b63f62ebe8df3181e5a1eb444b6ca55
Akshit Gupta <akshitgupta1996@gmail.com>
2019-02-24 15:14:08 -0800

    setup the layout component folder and added the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+
+import AppNavBar from "./components/layout/AppNavbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> Hello World</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavBar />
+          <div className="container">
+            <h1> Hello World</h1>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
