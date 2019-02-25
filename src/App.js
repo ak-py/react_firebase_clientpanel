c9ab309f17cb65e6c9a7bea8c8c74c1456b19bc6
Akshit Gupta <akshitgupta1996@gmail.com>
2019-02-24 20:47:10 -0800

    added firebase auth feautres for route redirection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { UserIsAuthenticated, UserIsNotAuthenticated } from "./helpers/auth";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -23,11 +23,31 @@ class App extends Component {
             <AppNavBar />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/client/add" component={AddClient} />
-                <Route exact path="/client/:id" component={ClientDetails} />
-                <Route exact path="/client/edit/:id" component={EditClient} />
+                <Route
+                  exact
+                  path="/"
+                  component={UserIsAuthenticated(Dashboard)}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  component={UserIsNotAuthenticated(Login)}
+                />
+                <Route
+                  exact
+                  path="/client/add"
+                  component={UserIsAuthenticated(AddClient)}
+                />
+                <Route
+                  exact
+                  path="/client/:id"
+                  component={UserIsAuthenticated(ClientDetails)}
+                />
+                <Route
+                  exact
+                  path="/client/edit/:id"
+                  component={UserIsAuthenticated(EditClient)}
+                />
               </Switch>
             </div>
           </div>
