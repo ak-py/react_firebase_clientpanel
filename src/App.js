d6da4284f62f738e74a134be8c7b44626334da3d
Akshit Gupta <akshitgupta1996@gmail.com>
2019-02-24 23:08:28 -0800

    completed register functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import AddClient from "./components/clients/AddClient";
 import ClientDetails from "./components/clients/ClientDetails";
 import EditClient from "./components/clients/EditClient";
 import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import Settings from "./components/settings/Settings";
 
 import "./App.css";
@@ -30,11 +31,6 @@ class App extends Component {
                 />
                 <Route
                   exact
-                  path="/login"
-                  component={UserIsNotAuthenticated(Login)}
-                />
-                <Route
-                  exact
                   path="/client/add"
                   component={UserIsAuthenticated(AddClient)}
                 />
@@ -52,6 +48,16 @@ class App extends Component {
                   exact
                   path="/settings"
                   component={UserIsAuthenticated(Settings)}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  component={UserIsNotAuthenticated(Login)}
+                />
+                <Route
+                  exact
+                  path="/register"
+                  component={UserIsNotAuthenticated(Register)}
                 />
               </Switch>
             </div>
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -6,11 +6,19 @@ import { firebaseConnect } from "react-redux-firebase";
 import { notifyUser } from "../../actions/notifyActions";
 import Alert from "../layout/Alert";
 
-class Login extends Component {
+class Register extends Component {
   state = {
     email: "",
     password: ""
   };
+
+  componentWillMount() {
+    const { allowRegistration } = this.props.settings;
+
+    if (!allowRegistration) {
+      this.props.history.push("/");
+    }
+  }
 
   onSubmit = e => {
     e.preventDefault();
@@ -18,12 +26,10 @@ class Login extends Component {
     const { firebase, notifyUser } = this.props;
     const { email, password } = this.state;
 
+    // Register with Firebase
     firebase
-      .login({
-        email,
-        password
-      })
-      .catch(err => notifyUser("Invalid Login Credentials", "error"));
+      .createUser({ email, password })
+      .catch(err => notifyUser("User Already Exists", "error"));
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -41,7 +47,7 @@ class Login extends Component {
               <h1 className="text-center pb-4 pt-3">
                 <span className="text-primary">
                   <i className="fas fa-lock" />
-                  {"  "}Login
+                  {"  "}Register
                 </span>
               </h1>
               <form onSubmit={this.onSubmit}>
@@ -69,7 +75,7 @@ class Login extends Component {
                 </div>
                 <input
                   type="submit"
-                  value="Login"
+                  value="Register"
                   className="btn btn-primary btn-block"
                 />
               </form>
@@ -81,7 +87,7 @@ class Login extends Component {
   }
 }
 
-Login.propTypes = {
+Register.propTypes = {
   firebase: PropTypes.object.isRequired,
   notify: PropTypes.object.isRequired,
   notifyUser: PropTypes.func.isRequired
@@ -91,8 +97,9 @@ export default compose(
   firebaseConnect(),
   connect(
     (state, props) => ({
-      notify: state.notify
+      notify: state.notify,
+      settings: state.settings
     }),
     { notifyUser }
   )
-)(Login);
+)(Register);
