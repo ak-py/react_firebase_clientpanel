22c8df70c86874b52f8057c59e3e849822f0598b
Akshit Gupta <akshitgupta1996@gmail.com>
2019-02-24 18:57:06 -0800

    complete delete functionality in client details page

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -25,8 +25,14 @@ class ClientDetails extends Component {
 
     // Update in firestore
     firestore.update({ collection: "clients", doc: client.id }, clientUpdate);
+  };
 
-    console.log(this.state.balanceUpdateAmount);
+  // Delete Client
+  onDeleteClick = e => {
+    const { firestore, client, history } = this.props;
+    firestore
+      .delete({ collection: "clients", doc: client.id })
+      .then(history.push("/"));
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -80,7 +86,9 @@ class ClientDetails extends Component {
                 <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
                   Edit
                 </Link>
-                <button className="btn btn-danger">Delete</button>
+                <button onClick={this.onDeleteClick} className="btn btn-danger">
+                  Delete
+                </button>
               </div>
             </div>
           </div>
