6246b36d74373b0a2fdcb03321bbe4f72b5d17e6
Akshit Gupta <akshitgupta1996@gmail.com>
2019-02-25 00:14:24 -0800

    Updates

---
+++ precache-manifest.61bb60e2b90a27211d762c60fcb191b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a544fbe9e28262bb09f",
+    "url": "/react_firebase_clientpanel/static/js/runtime~main.0a544fbe.js"
+  },
+  {
+    "revision": "4739ec0dd13902f63373",
+    "url": "/react_firebase_clientpanel/static/js/main.4739ec0d.chunk.js"
+  },
+  {
+    "revision": "646dc153a6373204d17a",
+    "url": "/react_firebase_clientpanel/static/js/2.646dc153.chunk.js"
+  },
+  {
+    "revision": "4739ec0dd13902f63373",
+    "url": "/react_firebase_clientpanel/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "82d40175de4d034b84cb158dd19dd9d0",
+    "url": "/react_firebase_clientpanel/index.html"
+  }
+];
