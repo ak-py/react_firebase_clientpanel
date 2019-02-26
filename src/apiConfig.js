9505ace2f94b02059ef7820e918e0c86777c1c39
Akshit Gupta <akshitgupta1996@gmail.com>
2019-02-26 02:36:52 -0800

    adding apiConfig file to git repo

---
+++ src/apiConfig.js
@@ -0,0 +1,8 @@
+export const apiConfig = {
+  apiKey: "AIzaSyABk0nMJCA-c8CeeL-yR4q8giI1TBhZZYo",
+  authDomain: "reactclientpanelak.firebaseapp.com",
+  databaseURL: "https://reactclientpanelak.firebaseio.com",
+  projectId: "reactclientpanelak",
+  storageBucket: "reactclientpanelak.appspot.com",
+  messagingSenderId: "575331541182"
+};
